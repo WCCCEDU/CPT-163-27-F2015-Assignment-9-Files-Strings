10ac9368ff163858ed64df0b36544ff8a98919b2
Gary A. Newsome <garyanewsome@gmail.com>
2015-12-02 19:37:39 -0500

    TryCatch_Fix
    Rewrote Try Catch statement to enclose only the statement and not the
    block

--- main/src/main/main.java
+++ main/src/main/main.java
@@ -30,70 +30,72 @@ public class main {
     diceReader.read();
 
     // create and open an outputfile
-    FileWriter rolledDice;
+    FileWriter rolledDice = null;
+    PrintWriter outputFile = null;
     try {
       rolledDice = new FileWriter("RolledDice.txt", true);
-      PrintWriter outputFile = new PrintWriter(rolledDice);
+      outputFile = new PrintWriter(rolledDice);
+    } catch (IOException ex) {
+      Logger.getLogger(main.class.getName()).log(Level.SEVERE, null, ex);
+    }
+    // create an array list and store the lines from the file in the list
+    List<String> lines = new ArrayList<>();
+    lines = diceReader.getLines();
 
-      // create an array list and store the lines from the file in the list
-      List<String> lines = new ArrayList<>();
-      lines = diceReader.getLines();
+    // tokenize the file first by 'space' then by ':'
+    for (int i = 0; i < lines.size(); i++) {
+      int trayValue = 0;
 
-      // tokenize the file first by 'space' then by ':'
-      for (int i = 0; i < lines.size(); i++) {
-        int trayValue = 0;
+      System.out.println("Tray #" + (i + 1) + ":");
+      outputFile.println("Tray #" + (i + 1) + ":");
+      String[] fileLine = lines.get(i).split(" ");
 
-        System.out.println("Tray #" + (i + 1) + ":");
-        outputFile.println("Tray #" + (i + 1) + ":");
-        String[] fileLine = lines.get(i).split(" ");
+      // seperate the file's line into die
+      for (int ii = 0; ii < fileLine.length; ii++) {
+        String[] dieConfig = fileLine[ii].split(":");
 
-        // seperate the file's line into die
-        for (int ii = 0; ii < fileLine.length; ii++) {
-          String[] dieConfig = fileLine[ii].split(":");
+        int numberOfSides = Integer.parseInt(dieConfig[0]);
+        String dieType = dieConfig[1];
 
-          int numberOfSides = Integer.parseInt(dieConfig[0]);
-          String dieType = dieConfig[1];
-
-          // if "loaded" - load the die
-          int loadedSide = 0;
-          if (dieType.equals("loaded")) {
-            loadedSide = Integer.parseInt(dieConfig[2]);
-          }
-
-          switch (dieType) {
-            case "numbered":
-              dice.NumberedDie numberedDie = new dice.NumberedDie(numberOfSides);
-              numberedDie.roll();
-              int numberedDieValue = numberedDie.getValue();
-              trayValue += numberedDieValue;
-              System.out.println("Numbered die roll: " + numberedDieValue);
-              outputFile.println("Numbered die roll: " + numberedDieValue);
-              break;
-            case "loaded":
-              dice.Loaded loaded = new dice.Loaded();
-              trayValue += loadedSide;
-              System.out.println("Loaded die roll: " + loadedSide);
-              outputFile.println("Loaded die roll: " + loadedSide);
-              break;
-            case "fudge":
-              dice.Fudge fudge = new dice.Fudge(numberOfSides);
-              fudge.roll();
-              int fudgeDieValue = fudge.getValue();
-              trayValue += fudgeDieValue;
-              System.out.println("Fudge die roll: " + fudgeDieValue);
-              outputFile.println("Fudge die roll: " + fudgeDieValue);
-              break;
-
-          }
+        // if "loaded" - load the die
+        int loadedSide = 0;
+        if (dieType.equals("loaded")) {
+          loadedSide = Integer.parseInt(dieConfig[2]);
         }
 
-        System.out.println("Total this tray: " + trayValue + "\n");
-        outputFile.println("Total this tray: " + trayValue + "\n");
-        
+        switch (dieType) {
+          case "numbered":
+            dice.NumberedDie numberedDie = new dice.NumberedDie(numberOfSides);
+            numberedDie.roll();
+            int numberedDieValue = numberedDie.getValue();
+            trayValue += numberedDieValue;
+            System.out.println("Numbered die roll: " + numberedDieValue);
+            outputFile.println("Numbered die roll: " + numberedDieValue);
+            break;
+          case "loaded":
+            dice.Loaded loaded = new dice.Loaded();
+            trayValue += loadedSide;
+            System.out.println("Loaded die roll: " + loadedSide);
+            outputFile.println("Loaded die roll: " + loadedSide);
+            break;
+          case "fudge":
+            dice.Fudge fudge = new dice.Fudge(numberOfSides);
+            fudge.roll();
+            int fudgeDieValue = fudge.getValue();
+            trayValue += fudgeDieValue;
+            System.out.println("Fudge die roll: " + fudgeDieValue);
+            outputFile.println("Fudge die roll: " + fudgeDieValue);
+            break;
 
+        }
       }
+
+      System.out.println("Total this tray: " + trayValue + "\n");
+      outputFile.println("Total this tray: " + trayValue + "\n");
+
+    }
+    try {
       rolledDice.close();
-      
     } catch (IOException ex) {
       Logger.getLogger(main.class.getName()).log(Level.SEVERE, null, ex);
     }
